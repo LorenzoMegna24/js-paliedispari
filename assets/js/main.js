dec4eff9184c79b2c1eb8f2881605b2c78c692e3
LorenzoMegna24 <megnalorenzo0@gmial.com>
2023-04-11 14:48:46 +0200

    Prima parte completata

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,44 @@
+// Palidroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+let nome = prompt("Scrivi il nome palindromo da verificare")
+console.log(nome);
+
+function ruotaNome( nome ){
+  let parolaRibaltata = ''
+
+  for ( let i = nome.length - 1; i >= 0; i-- ){
+    parolaRibaltata += nome[i]
+  }
+  return parolaRibaltata
+}
+
+let nomeRuotato = ruotaNome(nome)
+
+if (nome == nomeRuotato){
+  console.log(`il nome ${nome} è palindromo`);
+}else{
+  console.log(`il nome ${nome} non è palindromo`);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Pari e Dispari
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
